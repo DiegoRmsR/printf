5d7b89e2f25eb20ff2df4c3fb5ae5cf781f3ead1
JUCORTESA <984@holbertonschool.com>
2019-07-29 21:24:55 +0000

    [Fix] issue 1: condition on null and if print %r

--- _printf.c
+++ _printf.c
@@ -10,15 +10,13 @@ int _printf(const char *format, ...)
 	va_list list;
 	int i = 0, length = 0;
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && !format[1]))
 		return (-1);
 	va_start(list, format);
 		for (i = 0; format[i]; i++)
 		{
 			if (format[i] == '%')
 			{
-				if (!format[i + 1])
-					return(-1);
 				length += get_print_func(format[i + 1], list);
 				i++;
 			}
