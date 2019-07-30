5cf318c1fd3bd34799ba964e01c6a24ad693fdc0
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-30 20:49:33 +0000

    Update: issue #14 Function that prints the reversed string

---
+++ _printf_rev.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * print_rev - reverse the content of an array
+ * @list: string to print size
+ *
+ * Return: number of chars printed
+*/
+int print_rev(va_list list)
+{
+	char *str;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int tmp;
+	unsigned int s;
+	int count = 0;
+
+	str = va_arg(list, char*);
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i; j > 0; j--)
+	{
+		tmp = str[j - 1];
+		_putchar(tmp);
+		count++;
+
+	}
+	return (count);
+}
+
