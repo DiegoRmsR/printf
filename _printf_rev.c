1326eae8a65d50a88a0c095658b9ad5821f089b4
JUCORTESA <984@holbertonschool.com>
2019-07-30 21:07:43 +0000

    [Fix] issue #14 delete unused variable

--- _printf_rev.c
+++ _printf_rev.c
@@ -10,8 +10,7 @@ int print_rev(va_list list)
 	char *str;
 	unsigned int i = 0;
 	unsigned int j = 0;
-	unsigned int tmp;
-	unsigned int s;
+	unsigned int rev;
 	int count = 0;
 
 	str = va_arg(list, char*);
@@ -25,8 +24,8 @@ int print_rev(va_list list)
 
 	for (j = i; j > 0; j--)
 	{
-		tmp = str[j - 1];
-		_putchar(tmp);
+		rev = str[j - 1];
+		_putchar(rev);
 		count++;
 
 	}
